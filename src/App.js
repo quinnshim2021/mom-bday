3632b3f3bbe1399beb4cc496864c8b0a153d88c8
quinnshim2021 <quinnshim2021@u.northwestern.edu>
2021-05-09 23:46:51 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { pics } from './shared/pics';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import {useState} from 'react';
+
 
 function App() {
+  const [picUri, setPicUri] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="App-header">
+        <h1 className="header">We love you, Mom!</h1>
+        <div>
+            <img className="pics" src={pics[picUri].uri} onClick={() => {
+              /* 17 photos, so update if I add more */
+              const num = picUri === 16 ? 0 : picUri + 1;
+              setPicUri(num);
+            }} />
+        </div>
+        <p>Thank you for everything you do for us! We're so lucky to have you as a mother and a best friend.
+           We all wish we could be there with you every day, but we will always be thinking of you no matter what.
+           I love you so much! 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        <p>Here's some music that makes me think of you! Hope it brightens your day!</p>
+        <iframe src="https://open.spotify.com/embed/playlist/6s2ZphIQ3VstsqzkfIyx25" width="300" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </div>
     </div>
   );
 }
---
+++ src/shared/pics.js
@@ -0,0 +1,71 @@
+import o1 from '../assets/1.jpeg';
+import o2 from '../assets/2.jpeg';
+import o3 from '../assets/3.jpeg';
+import o4 from '../assets/4.jpeg';
+import o5 from '../assets/5.jpeg';
+import o6 from '../assets/6.jpeg';
+import o7 from '../assets/7.jpeg';
+import o8 from '../assets/8.jpeg';
+import o9 from '../assets/9.jpeg';
+import o10 from '../assets/10.jpeg';
+import o11 from '../assets/11.jpeg';
+import o12 from '../assets/12.jpeg';
+import o13 from '../assets/13.jpeg';
+import o14 from '../assets/14.jpeg';
+import o15 from '../assets/15.jpeg';
+import o16 from '../assets/16.jpeg';
+import o17 from '../assets/17.jpeg';
+
+export const pics = [
+    {
+        "uri": o1
+    },
+    {
+        "uri": o2
+    },
+    {
+        "uri": o3
+    },
+    {
+        "uri": o4
+    },
+    {
+        "uri": o5
+    },
+    {
+        "uri": o6
+    },
+    {
+        "uri": o7
+    },
+    {
+        "uri": o8
+    },
+    {
+        "uri": o9
+    },
+    {
+        "uri": o10
+    },
+    {
+        "uri": o11
+    },
+    {
+        "uri": o12
+    },
+    {
+        "uri": o13
+    },
+    {
+        "uri": o14
+    },
+    {
+        "uri": o15
+    },
+    {
+        "uri": o16
+    },
+    {
+        "uri": o17
+    }
+];
